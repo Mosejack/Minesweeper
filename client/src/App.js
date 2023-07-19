21f8ddb943327f5805852374fc30eb3e9db947e6
mosejack <moseja0147@comcast.net>
2023-07-18 23:12:50 -0700

    upadate

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import app from'./App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> MineSweeper</h1>
+      <div className= "minecontainer">
+        
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/board.js
@@ -0,0 +1,15 @@
+import React,  from 'react';
+
+function board(props)  {
+    let board = []
+
+    for
+        return (
+            <div>
+                
+            </div>
+        );
+    
+}
+
+export default board;
